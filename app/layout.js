1245a18dfc5f97f815e1cbbcfb03c129ab18f6ef
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-12 13:19:46 -0500

    cleaned up root layout

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,9 +20,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {children}
+      <body>
+        <Navbar />
+        <main>{children}</main>
       </body>
     </html>
   );
