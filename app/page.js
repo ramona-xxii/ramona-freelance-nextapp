a7738a6e68bf04cec16624df6a9429818a3278d9
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-12 12:46:13 -0500

    added basic html structure for navigation & sections

--- app/page.js
+++ app/page.js
@@ -1,8 +1,33 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <h1 className="bg-red-500 text-white">Test</h1>
+    <>
 
+    {/* desktop navigation */}
+    <nav>
+      <ul>
+        <li><a href="#home">Ramona</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+
+    <main>
+      <section id="home">
+        <h1>Your one-stop-shop for branding & website development!</h1>
+        <h2>Services</h2>
+
+      </section>
+
+      <section id="about">
+        <h1>Meet Your Developer</h1>
+      </section>
+
+      <section id="contact">
+        <h1>Get In Touch</h1>
+      </section>
+
+    </main>
+
+    </>
   );
 }
