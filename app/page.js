13efbfcaacdbc706d6fcf137452a5c275c04a1fb
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-12 13:20:27 -0500

    moved nav section to components folder

--- app/page.js
+++ app/page.js
@@ -2,16 +2,6 @@ export default function Home() {
   return (
     <>
 
-    {/* desktop navigation */}
-    <nav>
-      <ul>
-        <li><a href="#home">Ramona</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
-    </nav>
-
-    <main>
       <section id="home">
         <h1>Your one-stop-shop for branding & website development!</h1>
         <h2>Services</h2>
@@ -25,8 +15,6 @@ export default function Home() {
       <section id="contact">
         <h1>Get In Touch</h1>
       </section>
-
-    </main>
 
     </>
   );
