7a646f0954d04959c0458a8edf08d4f62b9bcacf
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-12 13:49:24 -0500

    added background colour

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,10 +1,10 @@
 export default function Navbar() {
     return (
-        <nav className="bg-blue-200 w-full sticky top-0">
+        <nav className="bg-[#1f48ff] w-full sticky top-0">
             <ul className="flex flex-row">
-                <li className="p-2"><a href="#home">Ramona</a></li>
-                <li className="p-2"><a href="#about">About</a></li>
-                <li className="p-2"><a href="#contact">Contact</a></li>
+                <li className="p-2 text-white"><a href="#home">Ramona</a></li>
+                <li className="p-2 text-white"><a href="#about">About</a></li>
+                <li className="p-2 text-white"><a href="#contact">Contact</a></li>
             </ul>
         </nav>
     );
