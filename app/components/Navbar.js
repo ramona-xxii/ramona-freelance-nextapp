04950cc1573409a9227fde358c494c5f11641942
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-13 14:48:49 -0500

    added logo to navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,11 +1,22 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+import Image from 'next/image';
+
+
 export default function Navbar() {
     return (
         <nav className="bg-[#f2f2f2] w-full sticky top-0 h-fit">
             <div className="flex items-center px-6">
                 {/* left side */}
-                <div>
-                    <a href="#home" className="py-4 px-2">NorthStar Studio</a>
-                    <span className="text-gray-300">by Ramona McBrearty</span>
+                <div className='flex'>
+                    <a href="#home">
+                        <Image
+                        src="/images/NorthStar.png"
+                        alt='northstar studio logo'
+                        width={200}
+                        height={200}/>
+                    </a>
                 </div>
                 {/* right side */}
                 <div className="flex ml-auto">
@@ -14,6 +25,10 @@ export default function Navbar() {
                     <a href="#contact" className="py-4 px-6">Contact</a>
                 </div>
             </div>
+            {/* hamburger button */}
+                <button type="button">
+                    <FontAwesomeIcon icon={faBars} className='h-8 w-8'/>
+                </button>
         </nav>
     );
 }
