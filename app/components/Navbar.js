4dfda6623f640c4ecc42bc973ad2aabde3a18000
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-13 13:37:24 -0500

    added 'services' link to navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,15 +1,17 @@
 export default function Navbar() {
     return (
         <nav className="bg-[#f2f2f2] w-full sticky top-0 h-fit">
-            <div className="flex px-2 items-center">
+            <div className="flex items-center px-6">
                 {/* left side */}
                 <div>
-                    <a href="#home" className="py-4 px-2">Ramona</a>
+                    <a href="#home" className="py-4 px-2">NorthStar Studio</a>
+                    <span className="text-gray-300">by Ramona McBrearty</span>
                 </div>
                 {/* right side */}
                 <div className="flex ml-auto">
-                    <a href="#about" className="py-4 px-2">About</a>
-                    <a href="#contact" className="py-4 px-2">Contact</a>
+                    <a href="#services" className="py-4 px-6">Services</a>
+                    <a href="#about" className="py-4 px-6">About</a>
+                    <a href="#contact" className="py-4 px-6">Contact</a>
                 </div>
             </div>
         </nav>
