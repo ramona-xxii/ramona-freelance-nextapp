3125b72e333c7c50a3db33834b77a187d08e72a1
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-14 18:21:09 -0500

    removed placeholder headings for blank slate

--- app/page.js
+++ app/page.js
@@ -3,18 +3,12 @@ export default function Home() {
     <>
 
       <section id="home">
-        <h1>Build Your Brand</h1>
-        <h2>Services</h2>
-    
-
       </section>
 
       <section id="about">
-        <h1>Meet Your Designer</h1>
       </section>
 
       <section id="contact">
-        <h1>Get In Touch</h1>
       </section>
 
     </>
