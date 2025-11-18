6a88cb9555e1c5ca534dffdc710b9f1936ffbb63
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-18 17:00:08 -0500

    added fixed card w-80

--- app/components/ServiceCard.js
+++ app/components/ServiceCard.js
@@ -2,8 +2,8 @@ export default function ServiceCard({title, description}) {
     
     return(
 
-        <div className="bg-purple-300 rounded-lg px-5 py-4">
-            <h1 className="text-center text-base sm:text-lg md:text-xl">{title}</h1>
+        <div className="bg-purple-300 rounded-lg px-5 py-4 w-75">
+            <h1 className="text-center">{title}</h1>
             <p className="py-2">{description}</p>
             <button className="w-full border border-black rounded-lg px-3 py-1.5">Button</button>
         </div>
