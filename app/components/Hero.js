be1c8363d05e0db43aa487e6bc8327af077b3a30
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-18 15:08:44 -0500

    changed placeholder description

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -8,7 +8,7 @@ export default function Hero() {
             {/* hero display */}
             <div id="hero" className="flex-col pt-5 space-y-3">
                 <h1 className="text-2xl sm:text-3xl md:text-4xl text-center">Elevate Your Brand with NorthStar Studio</h1>
-                <p className="text-lg md:text-xl text-center">Helping you build your business, from one entrepreneur to another.</p>
+                <p className="text-lg md:text-xl text-center">Here to help you build your business, from one entrepreneur to another.</p>
             </div>
         </Section>
     );
