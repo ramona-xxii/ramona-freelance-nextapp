edac9700d863289e2c88d7c94b50d65ce843e78a
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-18 15:08:29 -0500

    removed extra wrapping section mistake

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 import Hero from "./components/Hero";
-import Section from "./components/Section";
 import Services from "./components/Services";
 
 export default function Home() {
@@ -7,17 +6,9 @@ export default function Home() {
     <>
     {/* site container */}
     <div id="site-container" className="bg-blue-200 px-10">
-      <Section><Hero /></Section>
-      <Section><Services /></Section>
+      <Hero />
+      <Services />
 
-      {/* ABOUT section */}
-      <section id="about" className="bg-green-200">
-        <h1 className="text-2xl sm:text-3xl md:text-4xl text-center">Meet Your Developer</h1>
-      </section>
-
-      {/* CONTACT section */}
-      <section id="contact">
-      </section>
     </div>
       
     </>
