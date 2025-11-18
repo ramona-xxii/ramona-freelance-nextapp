172d261cb316b951b9cdfa9a5250ff39cea3059e
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-18 14:46:43 -0500

    added ServicesGrid to services component

--- app/components/Services.js
+++ app/components/Services.js
@@ -1,9 +1,11 @@
 import Section from "./Section";
+import ServicesGrid from "./ServicesGrid";
 
 export default function Services() {
     return (
-        <Section className="bg-yellow-200">
-            <h1>What Can I Help You With?</h1>
+        <Section id="services" className="bg-yellow-200">
+            <h1 className="text-center">Choose Your Service</h1>
+            <ServicesGrid />
         </Section>
     );
 }
