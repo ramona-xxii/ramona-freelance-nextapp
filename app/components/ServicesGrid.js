f5a130b5e8b2362f17b9245733419270e3e3f686
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-18 15:09:19 -0500

    changed placeholder description for card element

--- app/components/ServicesGrid.js
+++ app/components/ServicesGrid.js
@@ -5,7 +5,7 @@ export default function ServicesGrid() {
     // array of services
     const services = [
         { title: 'Logo', description: 'Personalize your business with an eye-catching logo' },
-        { title: 'Business Cards', description: 'Expand your client base with professional business cards' },
+        { title: 'Business Cards', description: 'Attract new clients with professional business cards' },
         { title: 'Website', description: 'Create a unique experience with a fully customized website' }
     ];
 
