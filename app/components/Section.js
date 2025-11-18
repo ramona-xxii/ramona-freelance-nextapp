3329cc78d34a255f7fd4cf18c50d438ae1c085fd
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-18 13:42:07 -0500

    slightly decreased vertical spacing between sections

--- app/components/Section.js
+++ app/components/Section.js
@@ -2,7 +2,7 @@ export default function Section({ children, id, className="" }) {
     return(
         <section 
             id={id}
-            className={`py-15 sm:py-20 md:py-25 lg:py-30 xl:py-35 2xl:py-40 ${className}`}
+            className={`py-10 sm:py-15 md:py-20 lg:py-25 xl:py-30 2xl:py-35 ${className}`}
         >
             {children}
         </section>
