f4e7f8baa1b32e88b81ba2621f4badf0d5dc3a55
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-18 15:08:59 -0500

    removed accidental grid mistake

--- app/components/ServiceCard.js
+++ app/components/ServiceCard.js
@@ -2,13 +2,11 @@ export default function ServiceCard({title, description}) {
     
     return(
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-5xl mx-auto">
-            <div className="bg-purple-300 rounded-lg px-5 py-4">
-              <h1 className="text-center text-base sm:text-lg md:text-xl">{title}</h1>
-              <p className="py-2">{description}</p>
-              <button className="w-full border border-black rounded-lg px-3 py-1.5">Button</button>
-            </div>
+        <div className="bg-purple-300 rounded-lg px-5 py-4">
+            <h1 className="text-center text-base sm:text-lg md:text-xl">{title}</h1>
+            <p className="py-2">{description}</p>
+            <button className="w-full border border-black rounded-lg px-3 py-1.5">Button</button>
         </div>
-
+        
     );
 }
