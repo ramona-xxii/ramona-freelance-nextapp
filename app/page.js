1c054e9ede60fee1cc324d88888baa62f3c4b182
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-18 14:45:56 -0500

    moved service section to components Services, ServiceCard, ServiceGrid

--- app/page.js
+++ app/page.js
@@ -10,32 +10,6 @@ export default function Home() {
       <Section><Hero /></Section>
       <Section><Services /></Section>
 
-      <section id="services">
-        <div id="services-grid" className="px-10 py-5">
-          {/* services grid */}
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-5xl mx-auto">
-            {/* card 1 - logo */}
-            <div className="bg-purple-300 rounded-lg px-5 py-4">
-              <h1 className="text-center text-base sm:text-lg md:text-xl">Logo</h1>
-              <p className="py-2">Make your brand stand out with an eye-catching logo</p>
-              <button className="w-full border border-black rounded-lg px-3 py-1.5">More Info</button>
-            </div>
-            {/* card 2 - business cards */}
-            <div className="bg-purple-300 rounded-lg px-5 py-4">
-              <h1 className="text-center text-base sm:text-lg md:text-xl">Business Cards</h1>
-              <p className="py-2">Expand your client base on the go with professional business cards</p>
-              <button className="w-full border border-black rounded-lg px-3 py-1.5">More Info</button>
-            </div>
-            {/* card 3 - website */}
-            <div className="bg-purple-300 rounded-lg px-5 py-4">
-              <h1 className="text-center text-base sm:text-lg md:text-xl">Website</h1>
-              <p className="py-2">Create a unique experience with a fully customized website</p>
-              <button className=" w-full border border-black rounded-lg px-3 py-1.5">More Info</button>
-            </div>
-          </div>
-        </div>
-      </section>
-
       {/* ABOUT section */}
       <section id="about" className="bg-green-200">
         <h1 className="text-2xl sm:text-3xl md:text-4xl text-center">Meet Your Developer</h1>
