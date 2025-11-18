25d81e78a3b7d6bdebb238a81cc09a50d8ff59f8
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-18 13:42:23 -0500

    created Services component

---
+++ app/components/Services.js
@@ -0,0 +1,9 @@
+import Section from "./Section";
+
+export default function Services() {
+    return (
+        <Section className="bg-yellow-200">
+            <h1>What Can I Help You With?</h1>
+        </Section>
+    );
+}
