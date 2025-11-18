43d0a4adf695121a0a66414aa6571920065e5e0b
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-18 17:00:25 -0500

    set h1 text-3xl

--- app/components/Services.js
+++ app/components/Services.js
@@ -4,7 +4,7 @@ import ServicesGrid from "./ServicesGrid";
 export default function Services() {
     return (
         <Section id="services" className="bg-yellow-200">
-            <h1 className="text-center">Choose Your Service</h1>
+            <h1 className="text-center text-3xl">Choose Your Service</h1>
             <ServicesGrid />
         </Section>
     );
