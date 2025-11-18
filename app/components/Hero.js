9a192c74c348a43cece8cd36e0dbe8c2511b6d64
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-18 13:41:35 -0500

    changed temp bg for layout visualization

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -4,7 +4,7 @@ export default function Hero() {
 
     return(
 
-        <Section id="home" className="bg-blue-400">
+        <Section id="home" className="bg-blue-300">
             {/* hero display */}
             <div id="hero" className="flex-col pt-5 space-y-3">
                 <h1 className="text-2xl sm:text-3xl md:text-4xl text-center">Elevate Your Brand with NorthStar Studio</h1>
