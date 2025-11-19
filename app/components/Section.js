846e2c16433ed1fa48c5c8c99673d39e709d0a07
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-19 14:03:54 -0500

    removed jumpy vertical spacing

--- app/components/Section.js
+++ app/components/Section.js
@@ -3,7 +3,7 @@ export default function Section({ children, id, className="" }) {
     return(
         <section 
             id={id}
-            className={`py-10 sm:py-15 md:py-20 lg:py-25 xl:py-30 2xl:py-35 ${className} scroll-mt-12 sm:scroll-mt-19`}
+            className={`${className} scroll-mt-12 sm:scroll-mt-19`}
         >
             {children}
         </section>
