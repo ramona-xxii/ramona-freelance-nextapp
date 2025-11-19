3065e6059e1527c9eff93d6e5b338060d4930759
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-19 14:03:28 -0500

    adjusted min-h-[70vh]

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -4,9 +4,9 @@ export default function Hero() {
 
     return(
 
-        <Section id="home" className="bg-blue-300">
+        <Section id="home" className="bg-blue-300 flex items-center justify-center min-h-[70vh] sm:min-h-[80vh]">
             {/* hero display */}
-            <div id="hero" className="flex-col pt-5 space-y-3">
+            <div id="hero" className="flex-col">
                 <h1 className="text-2xl sm:text-3xl md:text-4xl text-center">Elevate Your Brand with NorthStar Studio</h1>
                 <p className="text-lg md:text-xl text-center">Here to help you build your business, from one entrepreneur to another.</p>
             </div>
