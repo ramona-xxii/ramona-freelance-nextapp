63b2411235779f3f1c72ae6667c476ff1b4b5f14
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-19 12:45:27 -0500

    scroll-mt for desktop & mobile added

--- app/components/Section.js
+++ app/components/Section.js
@@ -1,8 +1,9 @@
 export default function Section({ children, id, className="" }) {
+    
     return(
         <section 
             id={id}
-            className={`py-10 sm:py-15 md:py-20 lg:py-25 xl:py-30 2xl:py-35 ${className}`}
+            className={`py-10 sm:py-15 md:py-20 lg:py-25 xl:py-30 2xl:py-35 ${className} scroll-mt-12 sm:scroll-mt-19`}
         >
             {children}
         </section>
