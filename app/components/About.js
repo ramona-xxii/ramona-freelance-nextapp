6ac14cfcd6a5125070f5c549bb7d2956ded8cd14
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-19 12:08:23 -0500

    initial commit for about section creation

---
+++ app/components/About.js
@@ -0,0 +1,10 @@
+import Section from "./Section";
+
+export default function About () {
+
+    return (
+        <Section id="about">
+            <h1>Meet Your Developer</h1>
+        </Section>
+    );
+}
