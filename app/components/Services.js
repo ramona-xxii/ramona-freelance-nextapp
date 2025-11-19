96115849f4477894382cee368bbc30a6e08897d9
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-19 14:04:13 -0500

    centered services grid on desktop & mobile

--- app/components/Services.js
+++ app/components/Services.js
@@ -3,9 +3,10 @@ import ServicesGrid from "./ServicesGrid";
 
 export default function Services() {
     return (
-        <Section id="services" className="bg-yellow-200">
-            <h1 className="text-center text-3xl">Choose Your Service</h1>
+        <Section id="services" className="bg-yellow-200 flex-col items-center justify-center">
+            <h1 className="text-center text-xl sm:text-3xl py-5">Choose A Service</h1>
             <ServicesGrid />
+            
         </Section>
     );
 }
