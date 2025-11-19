0ba0c91913fe0be2536ae7e56e16eec1398ccd97
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-19 14:03:05 -0500

    styled h1 about title

--- app/components/About.js
+++ app/components/About.js
@@ -3,8 +3,8 @@ import Section from "./Section";
 export default function About () {
 
     return (
-        <Section id="about">
-            <h1>Meet Your Developer</h1>
+        <Section id="about" className="bg-green-200">
+            <h1 className="text-center text-xl sm:text-3xl py-5">Meet Your Developer</h1>
         </Section>
     );
 }
