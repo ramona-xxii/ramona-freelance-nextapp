7d879b34f87ddbbe57345322cce21f21c8ba9431
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-20 12:47:42 -0500

    added fixed  h-90

--- app/components/ServiceCard.js
+++ app/components/ServiceCard.js
@@ -2,7 +2,7 @@ export default function ServiceCard({title, description}) {
     
     return(
 
-        <div className="bg-purple-300 rounded-lg px-5 py-4 w-75">
+        <div className="bg-purple-300 rounded-lg px-5 py-4 w-75 h-90 content-end shadow-md">
             <h1 className="text-center">{title}</h1>
             <p className="py-2">{description}</p>
             <button className="w-full border border-black rounded-lg px-3 py-1.5">Button</button>
