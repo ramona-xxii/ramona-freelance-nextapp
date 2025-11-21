500e77be037fd637f6afa0b0b114762da17f5ed6
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-21 08:51:13 -0500

    deleted duplicate file

--- app/Footer.js
+++
@@ -1,8 +0,0 @@
-import { faCopyright } from "@fortawesome/free-regular-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-export default function Footer () {
-    return (
-        <footer className="text-center text-gray-400 py-5"><FontAwesomeIcon icon={ faCopyright }/> Ramona McBrearty 2025</footer>
-    );
-}
