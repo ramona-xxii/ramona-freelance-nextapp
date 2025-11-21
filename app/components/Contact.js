03a7efbf0b4252a92a3cc6eb9d4b1487a59d0978
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-21 08:54:53 -0500

    contact file creation

---
+++ app/components/Contact.js
@@ -0,0 +1,11 @@
+import Section from "./Section";
+
+export default function Contact () {
+    return (
+        <Section id="contact">
+            <h1>Get In Touch</h1>
+            <div>
+            </div>
+        </Section>
+    );
+}
