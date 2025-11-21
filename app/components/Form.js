f2801b94235dd8d548ee7458c3841d42cd250548
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-21 11:05:49 -0500

    import useState

--- app/components/Form.js
+++ app/components/Form.js
@@ -1,10 +1,17 @@
+import { useState } from "react";
+
 export default function Form () {
+
     return (
-        <form className="flex flex-col">
-            <input type="text" placeholder="Name" />
-            <input type="text" placeholder="Desired Service" />
+
+        
+
+        <form className="flex flex-col bg-neutral-50">
+            <input type="text" name="name" placeholder="Name" />
+            <input type="text" placeholder="Subject" />
             <input type="email" placeholder="your.email@mail.com" />
-            <input type="text" placeholder="message" />
+            <textarea placeholder="message"></textarea>
         </form>
+        
     );
 }
