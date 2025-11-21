c41a36bcd82f577cc74455093ce2be4f93b6b659
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-21 11:06:01 -0500

    added form to contact seciton

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -1,11 +1,14 @@
 import Section from "./Section";
+import Form from "./Form";
 
 export default function Contact () {
     return (
         <Section id="contact">
             <h1>Get In Touch</h1>
-            <div>
+            <div className="bg-orange-200 w-fit">
+                <Form />
             </div>
+            
         </Section>
     );
 }
