767ce1d12a626ceac9072c1115f260111b9b3b10
Ramona <ramona.mcbrearty@gmail.com>
2025-11-22 17:31:57 -0500

    import useState

--- app/components/Form.js
+++ app/components/Form.js
@@ -1,13 +1,15 @@
+'use client'
+
 import { useState } from "react";
 
 export default function Form () {
 
+    const [inputs, setInputs] = useState({});
+
     return (
 
-        
-
         <form className="flex flex-col bg-neutral-50">
-            <input type="text" name="name" placeholder="Name" />
+            <input type="text" name="name" value={inputs.name} placeholder="Name" />
             <input type="text" placeholder="Subject" />
             <input type="email" placeholder="your.email@mail.com" />
             <textarea placeholder="message"></textarea>
