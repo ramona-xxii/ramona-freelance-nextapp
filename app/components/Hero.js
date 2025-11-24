12a69270ee4d16db45f033b2adf3e118e4df7f76
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-24 13:28:00 -0500

    added quick CTA button

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -4,11 +4,12 @@ export default function Hero() {
 
     return(
 
-        <Section id="home" className="bg-blue-300 flex items-center justify-center min-h-[70vh] sm:min-h-[80vh]">
+        <Section id="home" className="bg-blue-300 flex flex-col items-center justify-center min-h-[70vh] sm:min-h-[80vh]">
             {/* hero display */}
-            <div id="hero" className="flex-col">
+            <div id="hero" className="flex flex-col gap-1 items-center justify-center">
                 <h1 className="text-2xl sm:text-3xl md:text-4xl text-center">Elevate Your Brand with NorthStar Studio</h1>
                 <p className="text-lg md:text-xl text-center">Here to help you build your business, from one entrepreneur to another.</p>
+                <button type="button" className="border border-solid rounded-full px-4 py-2">Get A Free Quote</button>
             </div>
         </Section>
     );
