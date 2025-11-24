812c70a16a79cb4849eaacfd38355a7d011eff79
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-24 12:25:02 -0500

    basic h1 styling

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -4,8 +4,11 @@ import Form from "./Form";
 export default function Contact () {
     return (
         <Section id="contact">
-            <h1>Get In Touch</h1>
-            <div className="bg-orange-200 w-fit">
+            <div className="flex flex-col justify-center items-center py-5 bg-yellow-100">
+                <h1 className="text-3xl">Get In Touch</h1>
+            </div>
+            
+            <div className="bg-orange-200">
                 <Form />
             </div>
             
