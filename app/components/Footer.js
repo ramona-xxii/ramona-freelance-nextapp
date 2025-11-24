00788ff7004394fcf625b2ec46bf590f1b9ed728
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-24 12:37:31 -0500

    fixed html structure & basic styling

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -3,6 +3,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Footer () {
     return (
-        <footer className="text-center text-gray-400 py-5"><FontAwesomeIcon icon={ faCopyright }/> Ramona McBrearty 2025</footer>
+        <footer className="text-gray-400 py-5">
+            <div className="flex flex-row justify-center items-center gap-1">
+                <FontAwesomeIcon icon={ faCopyright } className="w-4 h-4"/>
+                <h1>Ramona McBrearty 2025</h1>
+            </div>
+        </footer>
     );
 }
