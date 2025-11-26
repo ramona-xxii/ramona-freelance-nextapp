3dd9484bc83baf3256f075a916c99f887d54b582
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-26 13:51:59 -0500

    added async function

--- app/api/send-email/route.js
+++ app/api/send-email/route.js
@@ -1,2 +1,16 @@
-const nodemailer = require("nodemailer");
+import nodemailer from 'nodemailer';
+import { NextResponse } from 'next/server';
 
+export async function POST(req) {
+    
+    const { name, email, subject, message } = await req.json(); // 'req' contains info about incoming http request (headers, method, body)
+                                                                // must parse as json
+                                                                // extract 4 properties received & put into 4 objs: name, email, subject, message.
+    
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: { user: process.env.GMAIL_USER, pass: process.env.GMAIL_PASS }
+    });
+
+    
+}
