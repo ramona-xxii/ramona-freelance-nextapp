b4ea6b7231c54dc2185ad61ac3f483d5cc7a6a26
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-11-26 11:25:46 -0500

    change CTA button title

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -9,7 +9,7 @@ export default function Hero() {
             <div id="hero" className="flex flex-col gap-1 items-center justify-center">
                 <h1 className="text-2xl sm:text-3xl md:text-4xl text-center">Elevate Your Brand with NorthStar Studio</h1>
                 <p className="text-lg md:text-xl text-center">Here to help you build your business, from one entrepreneur to another.</p>
-                <button type="button" className="border border-solid rounded-full px-4 py-2">Get A Free Quote</button>
+                <button type="button" className="border border-solid rounded-full px-4 py-2">Let's Get Started</button>
             </div>
         </Section>
     );
